69f629bc46f13a2e09a594eac5ca693ea858fd28
Mustafa Ileri <mstfleri@gmail.com>
2011-12-10 00:43:09 +0200

    Framework benchmarks

---
+++ frameworks/kohana/3.0.9/application/logs/2011/11/25.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+2011-11-25 07:40:39 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: frameworks/kohana/3.0.9 ~ SYSPATH/classes/kohana/request.php [ 676 ]
+2011-11-25 08:11:56 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: frameworks/kohana/3.0.9 ~ SYSPATH/classes/kohana/request.php [ 676 ]
+2011-11-25 08:13:06 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: frameworks/kohana/3.0.9 ~ SYSPATH/classes/kohana/request.php [ 676 ]
+2011-11-25 08:13:17 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: frameworks/kohana/3.0.9 ~ SYSPATH/classes/kohana/request.php [ 676 ]
+2011-11-25 08:14:12 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: frameworks/kohana/3.0.9/index.php ~ SYSPATH/classes/kohana/request.php [ 676 ]
+2011-11-25 15:51:18 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: frameworks/kohana/3.0.9 ~ SYSPATH/classes/kohana/request.php [ 676 ]
+2011-11-25 15:52:31 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: frameworks/kohana/3.0.9 ~ SYSPATH/classes/kohana/request.php [ 676 ]
