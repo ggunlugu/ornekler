5050aa7b83b569c583e635ef93538c23f07bffbb
Osman Yuksel <yuxel@sonsuzdongu.com>
2011-12-26 10:50:02 +0200

    Namespace loading moved to application.ini from Application.php, and Application.php removed

--- zf-core/library/Base/Application.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'Zend/Application.php';
-require_once 'Zend/Loader/Autoloader.php';
-
-$loader = Zend_Loader_Autoloader::getInstance();
-$loader->registerNamespace('Base');
-$loader->registerNamespace('Entity');
--- zf-core/public/index.php
+++ zf-core/public/index.php
@@ -15,7 +15,7 @@ set_include_path(implode(PATH_SEPARATOR, array(
 )));
 
 /** Zend_Application */
-require_once 'Base/Application.php';
+require_once 'Zend/Application.php';
 
 // Create application, bootstrap, and run
 $application = new Zend_Application(
